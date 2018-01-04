41052046e278ca2add4598edbc806991b066702c
James Fong <jamesfong1996@gmail.com>
2018-01-04 10:49:31 -0500

    Initial commit

---
+++ Drips/Drips/Note/NoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteTableViewCell.swift
+//  Drips
+//
+//  Created by James Fong on 2018-01-04.
+//  Copyright © 2018 James Fong. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
